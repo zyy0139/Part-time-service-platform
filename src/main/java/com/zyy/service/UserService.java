24174edc7cf7a3074e2b90c66e6f158a252c9300
ZhangYY <1443802862@qq.com>
2024-02-16 20:02:54 +0800

    redis配置

--- src/main/java/com/zyy/service/UserService.java
+++ src/main/java/com/zyy/service/UserService.java
@@ -7,4 +7,5 @@ public interface UserService {
     public int UpdateAllById(String id);
     public Users SelectAllById(String id);
     public String SelectIdByEmail(String email);
+    public String SelectEmail(String email);
 }
--- src/main/java/com/zyy/service/impl/UserServiceImpl.java
+++ src/main/java/com/zyy/service/impl/UserServiceImpl.java
@@ -35,4 +35,9 @@ public class UserServiceImpl implements UserService {
         String id=userMapper.selectIdByEmail(email);
         return id;
     }
+
+    @Override
+    public String SelectEmail(String email) {
+        return null;
+    }
 }
