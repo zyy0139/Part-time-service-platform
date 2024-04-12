aa168ffb87bdc630b388f63444f6d85684e67227
ZhangYY <1443802862@qq.com>
2024-04-12 14:19:45 +0800

    封装获取招聘信息列表方法

--- src/main/java/com/zyy/controller/RecruitController.java
+++ src/main/java/com/zyy/controller/RecruitController.java
@@ -31,6 +31,9 @@ public class RecruitController {
     @Autowired
     private CompanyServiceImpl companyService;
 
+    @Autowired
+    private GetRecruitList getRecruitList;
+
     @PostMapping("/sendRecruit")
     public Result sendRecruit(@RequestBody String body, HttpServletRequest request){
         if(body==null){
@@ -201,30 +204,8 @@ public class RecruitController {
         if(list.getSize()==0){
             return ResponseUtils.failResult(ResultCode.select_fail,"暂无该公司的招聘信息");
         }
-        List<Map<String,Object>> mapList=new ArrayList<>();
-        List<Recruits> recruitsList=list.getList();
-        for(int i=0;i<list.getSize();i++){
-            Map<String,Object> map=new HashMap<>();
-            Companies company=companyService.selectAllById(recruitsList.get(i).getCompanyId());
-            map.put("recruitId",recruitsList.get(i).getRecruitId());
-            map.put("companyName",company.getName());
-            map.put("companyAddress",company.getAddress());
-            map.put("companyEmail",company.getEmail());
-            map.put("companyPhone",company.getPhone());
-            map.put("career",recruitsList.get(i).getCareer());
-            map.put("type",recruitsList.get(i).getType());
-            map.put("number",recruitsList.get(i).getNumber());
-            map.put("message",recruitsList.get(i).getMessage());
-            map.put("salary",recruitsList.get(i).getSalary());
-            map.put("freefl",recruitsList.get(i).isFreefl());
-            map.put("releaseDate",recruitsList.get(i).getReleaseDate());
-            mapList.add(map);
-        }
-        int total=recruitService.getNumByCompanyId(companyId);
-        Map<String,Object> map1=new HashMap<>();
-        map1.put("total",total);
-        map1.put("recruitList",mapList);
-        return ResponseUtils.successResult("查询成功",map1);
+        Map<String,Object> map=getRecruitList.getList(list,companyId);
+        return ResponseUtils.successResult("查询成功",map);
     }
 
     @GetMapping("/getByCompanyId")
@@ -237,30 +218,8 @@ public class RecruitController {
         if(list.getSize()==0){
             return ResponseUtils.failResult(ResultCode.select_fail,"暂无该公司的招聘信息");
         }
-        List<Map<String,Object>> mapList=new ArrayList<>();
-        List<Recruits> recruitsList=list.getList();
-        for(int i=0;i<list.getSize();i++){
-            Map<String,Object> map=new HashMap<>();
-            Companies company=companyService.selectAllById(recruitsList.get(i).getCompanyId());
-            map.put("recruitId",recruitsList.get(i).getRecruitId());
-            map.put("companyName",company.getName());
-            map.put("companyAddress",company.getAddress());
-            map.put("companyEmail",company.getEmail());
-            map.put("companyPhone",company.getPhone());
-            map.put("career",recruitsList.get(i).getCareer());
-            map.put("type",recruitsList.get(i).getType());
-            map.put("number",recruitsList.get(i).getNumber());
-            map.put("message",recruitsList.get(i).getMessage());
-            map.put("salary",recruitsList.get(i).getSalary());
-            map.put("freefl",recruitsList.get(i).isFreefl());
-            map.put("releaseDate",recruitsList.get(i).getReleaseDate());
-            mapList.add(map);
-        }
-        int total=recruitService.getNumByCompanyId(companyId);
-        Map<String,Object> map1=new HashMap<>();
-        map1.put("total",total);
-        map1.put("recruitList",mapList);
-        return ResponseUtils.successResult("查询成功",map1);
+        Map<String,Object> map=getRecruitList.getList(list,companyId);
+        return ResponseUtils.successResult("查询成功",map);
     }
 
 }
---
+++ src/main/java/com/zyy/utils/GetRecruitList.java
@@ -0,0 +1,49 @@
+package com.zyy.utils;
+
+import com.github.pagehelper.PageInfo;
+import com.zyy.entity.Companies;
+import com.zyy.entity.Recruits;
+import com.zyy.service.impl.CompanyServiceImpl;
+import com.zyy.service.impl.RecruitServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class GetRecruitList {
+
+    @Autowired
+    private RecruitServiceImpl recruitService;
+
+    @Autowired
+    private CompanyServiceImpl companyService;
+
+    public Map<String,Object> getList(PageInfo<Recruits> list,String companyId){
+        List<Map<String,Object>> mapList=new ArrayList<>();
+        List<Recruits> recruitsList=list.getList();
+        for(int i=0;i<list.getSize();i++){
+            Map<String,Object> map=new HashMap<>();
+            Companies company=companyService.selectAllById(recruitsList.get(i).getCompanyId());
+            map.put("recruitId",recruitsList.get(i).getRecruitId());
+            map.put("companyName",company.getName());
+            map.put("companyAddress",company.getAddress());
+            map.put("companyEmail",company.getEmail());
+            map.put("companyPhone",company.getPhone());
+            map.put("career",recruitsList.get(i).getCareer());
+            map.put("type",recruitsList.get(i).getType());
+            map.put("number",recruitsList.get(i).getNumber());
+            map.put("message",recruitsList.get(i).getMessage());
+            map.put("salary",recruitsList.get(i).getSalary());
+            map.put("freefl",recruitsList.get(i).isFreefl());
+            map.put("releaseDate",recruitsList.get(i).getReleaseDate());
+            mapList.add(map);
+        }
+        int total=recruitService.getNumByCompanyId(companyId);
+        Map<String,Object> map1=new HashMap<>();
+        map1.put("total",total);
+        map1.put("recruitList",mapList);
+        return map1;
+    }
+}
